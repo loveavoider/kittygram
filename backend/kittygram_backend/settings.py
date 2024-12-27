d90944ef15d4ebcdc8c7f10fd0c11bfbf31fd4ba
Pavlyuchenkov Evgeny <pavlyuchenkovevg@gmail.com>
2024-12-27 17:04:22 +0300

    fix after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = '/backend_static/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media'
